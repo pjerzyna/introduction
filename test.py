547a618dac2a29794b98d01d22b1fceb4801acc9
pjerzyna <pawel.jerzyna@interia.pl>
2023-09-22 15:23:56 +0200

    second commit

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("Hello")
+
+class NewAbility():
+    def __init__(self, voice):
+        self.voice = voice
+
+    def scream(self):
+        print(f"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA {self.voice}")
+
+cat = NewAbility("meow")
+cat.scream()
